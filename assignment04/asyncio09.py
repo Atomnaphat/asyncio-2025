e5634d57b77c7e5957c49da93276e35a651002c9
Atomnaphat <napat1970@gmail.com>
2025-06-25 11:58:02 +0700

    ส่ง assignment3-4

--- assignment04/asyncio09.py
+++ assignment04/asyncio09.py
@@ -1,1 +1,17 @@
 # cancel task
+import asyncio
+
+async def slow_task():
+    await asyncio.sleep(3)
+
+async def main():
+    task = asyncio.create_task(slow_task())
+    print("ยกเลิก task ใน 1 วิ")
+    await asyncio.sleep(1) # รอ 1 วินาที
+    task.cancel() # สั่งยกเลิก task
+    try:
+        await task # พยายามรอ task ที่ถูกยกเลิก
+    except asyncio.CancelledError:
+        print("Task ถูก cancel แล้ว")
+
+asyncio.run(main())
--- assignment04/asyncio13.py
+++ assignment04/asyncio13.py
@@ -1,1 +1,17 @@
 # all tasks
+import asyncio
+
+async def dummy():
+    await asyncio.sleep(2)
+
+async def main():
+    t1 = asyncio.create_task(dummy(), name="Task A")
+    t2 = asyncio.create_task(dummy(), name="Task B")
+    await asyncio.sleep(0.1) # ให้ tasks ได้เริ่มทำงานก่อน
+
+    all_tasks = asyncio.all_tasks() # ดึง tasks ทั้งหมดที่อยู่ใน Event Loop ปัจจุบัน
+    print("Task ทั้งหมดใน loop:")
+    for t in all_tasks:
+        print("-", t.get_name()) # พิมพ์ชื่อของแต่ละ task
+
+asyncio.run(main())
